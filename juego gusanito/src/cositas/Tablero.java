3d6c34f60ea2f0bb9f3878b3f387929e3ca618b5
Felurian <Felurian@users.noreply.github.com>
2017-09-07 22:43:34 -0600

    asdasadsatnd

--- juego gusanito/src/cositas/Tablero.java
+++ juego gusanito/src/cositas/Tablero.java
@@ -2,23 +2,36 @@ package cositas;
 import java.util.*;
 public class Tablero 
 {
-	public int matriz[][] = new int[4][4];
+	public int size = 4;
+	public int matriz[][] = new int[size][size];
 	
 	Tablero() {
-		LlenaMatriz();
+		llenaMatriz();
 	}
 	
-	Tablero(int[][] mat){
-		this.matriz = mat;
+	Tablero(Tablero t){ 		// copiar otro tablero
+		int[][] m = t.matriz;
+		for(int x=0; x < this.size; x++) {
+			for(int y=0; y < this.size; y++) {
+				this.matriz[x][y] = m[x][y];
+			}
+		}
 	}
 	
 	public static void main(String[] args) 
 	{ 
 		Tablero m = new Tablero();
-		m.matriz[2][2]=0;
-		m.ImprimeMatriz();
+		m.matriz[2][2]=1;
+		m.imprimeMatriz();
 		
-		m.MovAbajo(0,2);
+		
+		//m.MovAbajo(0, 2);
+		//List l = m.sucesor();
+		//System.out.println("tablero sucesor 1:");
+		//((Tablero)l.get(0)).imprimeMatriz();
+		//System.out.println("tablero sucesor 2:");
+		//((Tablero)l.get(1)).imprimeMatriz();
+		//m.MovAbajo(0,2);
 	}
 	
 	public boolean vacio(int x, int y)
@@ -35,7 +48,7 @@ public class Tablero
 		matriz[x][y+2]=1;//El nuevo
 		matriz[x][y]=0;//El que se mueve(Donde estaba)
 		matriz[x][y+1]=0;//El que se comi?
-		ImprimeMatriz();
+		//imprimeMatriz();
 		
 	}
 	public boolean puedeIzquierda(int x, int y) {
@@ -48,7 +61,7 @@ public class Tablero
 		matriz[x][y-2]=1;//El nuevo
 		matriz[x][y]=0;//El que se mueve(Donde estaba)
 		matriz[x][y-1]=0;//El que se comi?
-		ImprimeMatriz();
+		//imprimeMatriz();
 		
 	}
 	public boolean puedeArriba(int x, int y) {
@@ -61,7 +74,7 @@ public class Tablero
 		matriz[x-2][y]=1;//El nuevo
 		matriz[x][y]=0;//El que se mueve(Donde estaba)
 		matriz[x-1][y]=0;//El que se comi?
-		ImprimeMatriz();
+		//imprimeMatriz();
 		
 	}
 	public boolean puedeAbajo(int x, int y) {
@@ -74,42 +87,52 @@ public class Tablero
 		matriz[x+2][y]=1;//El nuevo
 		matriz[x][y]=0;//El que se mueve(Donde estaba)
 		matriz[x+1][y]=0;//El que se comi?
-		ImprimeMatriz();
+		//imprimeMatriz();
 		
 	}
-	public void Sucesor()
+	public List<Tablero> sucesor()
 	{
-		int x=matriz.length;
-		int y=matriz[0].length;
+		//int x=matriz.length;
+		//int y=matriz[0].length;
 		List<Tablero> l = new ArrayList<Tablero>();
-		for(int i=0; i<x ;i++)
+		for(int i=0; i<size ;i++)
 		{
-			for(int j=0; j<y ;j++)
+			for(int j=0; j<size ;j++)
 			{
 				if(matriz[i][j]==0) // cuadro vacio, no nos interesa
 					continue;
 				
 				if(puedeIzquierda(i,j)){
-					Tablero m2 = new Tablero(matriz); // creamos nueva matriz
+					//System.out.println("izq");
+					Tablero m2 = new Tablero(this); // creamos nueva matriz
 					m2.MovIzquierda(i, j);
-					l.add(m2);					// agregamos a la lista
+					l.add(m2);						// agregamos a la lista
 				}
 				if(puedeDerecha(i,j)){
-					Tablero m2 = new Tablero(matriz);
-					
+					//System.out.println("der");
+					Tablero m2 = new Tablero(this);
+					m2.MovDerecha(i, j);
+					l.add(m2);
 				}
 				if(puedeArriba(i,j)){
-					continue;
+					//System.out.println("arr");
+					Tablero m2 = new Tablero(this);
+					m2.MovArriba(i, j);
+					l.add(m2);
 				}
 				if(puedeAbajo(i,j)){
-					continue;
+					//System.out.println("aba");
+					Tablero m2 = new Tablero(this);
+					m2.MovAbajo(i, j);
+					l.add(m2);
 				}
 			}
 		}
+		return l;
 	}
 	
-	
-	public void LlenaMatriz()
+
+	public void llenaMatriz()
 	{
 		for(int i=0;i<matriz.length;i++)
 		{
@@ -120,7 +143,18 @@ public class Tablero
 		}
 	}
 	
-	public void ImprimeMatriz()
+	public void borraMatriz()
+	{
+		for(int i=0;i<matriz.length;i++)
+		{
+			for(int j=0;j<matriz.length;j++)
+			{
+				matriz[i][j]=0;
+			}
+		}
+	}
+	
+	public void imprimeMatriz()
 	{
 		for(int i=0;i<matriz.length;i++)
 		{
@@ -132,6 +166,34 @@ public class Tablero
 			System.out.println();
 		}
 		System.out.println("\t ------------");
+	}
+	
+	public boolean winner() {
+		int c = 0;
+		for(int i=0;i<matriz.length;i++)
+		{
+			for(int j=0;j<matriz.length;j++)
+			{
+				c += matriz[i][j];
+			}
+		}
+		return c == 1;
+	}
+	
+	public void ganar() {
+		Tablero winner = null; 
+		List<Tablero> s = this.sucesor();
+		Tablero[] ts = (Tablero[]) s.toArray();
+		for (Tablero tablero : ts) {
+			if(tablero.winner()) {
+				winner = tablero;
+				break;
+			}
+				
+		}
+		winner.imprimeMatriz();
+		System.out.println("ganamos!!!");
+		
 	}
 
 }

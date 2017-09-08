8409380825e7ae621f1ba1f2213646be5e349fdb
Felurian <Felurian@users.noreply.github.com>
2017-09-07 21:33:29 -0600

    puede arriba / abajo / etc

--- juego gusanito/src/cositas/Main.java
+++ juego gusanito/src/cositas/Main.java
@@ -2,77 +2,83 @@ package cositas;
 import java.util.*;
 public class Main 
 {
-	static int matriz[][] = new int[4][4];
+	public int matriz[][] = new int[4][4];
 	
 	Main() {
-		
+		LlenaMatriz();
+	}
+	
+	Main(int[][] mat){
+		this.matriz = mat;
 	}
 	
 	public static void main(String[] args) 
 	{ 
-		LlenaMatriz();
-		matriz[2][2]=0;
-		ImprimeMatriz();
-		System.out.print("Ingresa las coordenas");
+		Main m = new Main();
+		m.matriz[2][2]=0;
+		m.ImprimeMatriz();
 		
-		MovAbajo(0,2);
+		m.MovAbajo(0,2);
 	}
 	
 	
-	public static boolean vacio(int x, int y)
+	public boolean vacio(int x, int y)
 	{
 		return matriz[x][y] == 0;
 	}
-	
-	public static void MovDerecha(int x, int y)
+	public boolean puedeDerecha(int x, int y) {
+		return !(y>1 || vacio(x,y) || vacio(x,y+1) || !vacio(x,y+2));
+	}
+	public  void MovDerecha(int x, int y)
 	{
-		//Falla si:
-		if(y>1 || vacio(x,y) || vacio(x,y+1) || !vacio(x,y+2))
-		//			actual		el comido		el nuevo
-			return;//No se puede
+		if(!puedeDerecha(x,y))
+			return;
 		matriz[x][y+2]=1;//El nuevo
 		matriz[x][y]=0;//El que se mueve(Donde estaba)
 		matriz[x][y+1]=0;//El que se comi?
 		ImprimeMatriz();
 		
 	}
-	public static void MovIzquierda(int x, int y)
+	public boolean puedeIzquierda(int x, int y) {
+		return !(y<2 || vacio(x,y) || vacio(x,y-1) || !vacio(x,y-2));
+	}
+	public  void MovIzquierda(int x, int y)
 	{
-		//Falla si:
-		if(y<2 || vacio(x,y) || vacio(x,y-1) || !vacio(x,y-2))
-		//			actual		el comido		el nuevo
-			return;//No se puede
+		if(!puedeIzquierda(x,y))
+			return;
 		matriz[x][y-2]=1;//El nuevo
 		matriz[x][y]=0;//El que se mueve(Donde estaba)
 		matriz[x][y-1]=0;//El que se comi?
 		ImprimeMatriz();
 		
 	}
-	public static void MovArriba(int x, int y)
+	public boolean puedeArriba(int x, int y) {
+		return !(x<2 || vacio(x,y) || vacio(x-1,y) || !vacio(x-2,y));
+	}
+	public  void MovArriba(int x, int y)
 	{
-		//Falla si:
-		if(x<2 || vacio(x,y) || vacio(x-1,y) || !vacio(x-2,y))
-		//			actual		el comido		el nuevo
-			return;//No se puede
+		if(!puedeArriba(x, y))
+			return;
 		matriz[x-2][y]=1;//El nuevo
 		matriz[x][y]=0;//El que se mueve(Donde estaba)
 		matriz[x-1][y]=0;//El que se comi?
 		ImprimeMatriz();
 		
 	}
-	public static void MovAbajo(int x, int y)
+	public boolean puedeAbajo(int x, int y) {
+		return !(x>1 || vacio(x,y) || vacio(x+1,y) || !vacio(x+2,y));
+	}
+	public  void MovAbajo(int x, int y)
 	{
-		//Falla si:
-		if(x>1 || vacio(x,y) || vacio(x+1,y) || !vacio(x+2,y))
-		//			actual		el comido		el nuevo
-			return;//No se puede
+		if(!puedeAbajo(x,y))
+			return;
 		matriz[x+2][y]=1;//El nuevo
 		matriz[x][y]=0;//El que se mueve(Donde estaba)
 		matriz[x+1][y]=0;//El que se comi?
 		ImprimeMatriz();
 		
 	}
-	public static void Sucesor(int matriz[][])
+	public void Sucesor()
 	{
 		int x=matriz.length;
 		int y=matriz[0].length;
@@ -81,12 +87,8 @@ public class Main
 		{
 			for(int j=0; j<y ;j++)
 			{
-				if(matriz[i][j]==0)//Est? vacúŒ
-					continue; //me lo salto
-				if(matriz[i][j+1]==1 && matriz[i][j+2]==0)
-				{
-					
-				}
+				if(matriz[i][j]==0)//Esta? vacio
+					continue;
 			}
 		}
 	}
@@ -114,7 +116,7 @@ public class Main
 	
 	
 	
-	public static void LlenaMatriz()
+	public void LlenaMatriz()
 	{
 		for(int i=0;i<matriz.length;i++)
 		{
@@ -125,7 +127,7 @@ public class Main
 		}
 	}
 	
-	public static void ImprimeMatriz()
+	public void ImprimeMatriz()
 	{
 		for(int i=0;i<matriz.length;i++)
 		{
